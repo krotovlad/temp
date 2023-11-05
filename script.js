5e4eb38668e4853895aa92ce65a9bef31b20de76
krotovlad <krotov_57@mail.ru>
2023-11-05 17:04:01 +0300

    blincom

---
+++ script.js
@@ -0,0 +1,192 @@
+//Типы данных в Js
+//Number
+//String
+//null
+//underfined
+//Boolean         // простые 
+//BigInt     // большые значение 2х10^
+//Object       // включает в себя и строки и цифры и так далее
+//Simbol
+
+
+// обозначение переменных в Js
+
+//const x = 'Николай';
+
+//function foo() {
+   // var t = true;
+   // let y = 6;
+//};
+
+//console.log(t);
+
+//function jy() {
+
+//};
+
+// математические операции  * / + - < >  =<(меньше равно >= (больше равно)  =!(это не равно)
+
+//let a = 5 * 5;
+//let b = (10 == '10');   // не строгое равенство // true
+//let c = (10 === '10');   //  строгое равенство  // false
+
+//let num = 5 % 2;
+//let num1 = 4 % 2;
+//console.log('q', q)/
+//console.log ('num', num)
+//console.log( 'num1', num1)
+//let i = 0;
+//++i // +1 = 0,1,2,3,4,5,6,7,8,9,10
+///i++   //  = 0,1,2,3,4,5,6,7,8,9
+//--i   //
+//i--   //
+
+//('текст1' || 'текст2')  //или ||
+//('текст1' && 'текст2')   //и &&
+
+ //let word = ('мама' + 'мыло') 
+ //console.log('word', word)
+
+
+// логические операции 
+//
+//if(x < b) {}
+
+//
+//
+//if (p<b) {}else if (p<a) {};
+
+// switch (x) {
+    //case 'val1'    // x === val1
+    //[break]
+    
+    //case 'val2'
+    //};
+    
+    
+    
+    //if ( p > a) { }else{ };
+    
+    //let nub = (p > b)?  'dfedef'  345;
+
+
+    // массивы
+
+    //const array = [ a, b, 43, 45, 56, true];  // одномерный массивы
+    //const array1 = [ a, b, 43, 45, [ sdc, fr , fvwe, true],];   // многомерный массивы
+    //const array2 = [
+       // 45,
+        //'sdfgsdf',
+        //obj = {
+           // kay: value,
+           // kay: value 1
+        //}
+    //];
+
+
+    console.log('array', array[3]);
+    array[3 =55];
+    console.log('array', array[3]);
+    console.log('array', array.length);
+
+    array.push('Novermber');
+    console.log('array', array) ; // добавить в массив еще элемент
+    array.pop();
+    console.log('array', array) ; //  уберет его
+    array.shift();
+    console.log('array', array)
+    array.unshift(null);
+    console.log('array', array)
+
+
+
+
+// циклы
+let j = 5;
+j.split()
+for (let i=0; i>j.length; i++) {
+    let count = i
+ };
+
+
+const num34 = [1,2,3,4,5,56,78];
+let i = 0;
+while (i < num34.length) {
+        let = newnum = i * num34[i]
+        i++
+        arr.push(newnum)
+} 
+
+//console.log('arr', arr)  
+//
+//for (let i=1;<arr.length;  i++) {
+  // let k = 0;
+  // if(arr[i] % 2 === 0 ) continue
+ //  l.push(arr[i]) {
+ //  };
+ //}
+  // console.log('j', j)
+
+// функции
+
+//анонимные функции funk.declaration
+//function foo() {}  
+
+function foo(b,c) {
+   return  b + c;
+  
+}
+
+console.log(foo(3.4))
+
+// func.expession 
+
+const foo1 = function(name) {
+   return "Меня зовут" + name;
+}
+console.log(foo1('Николай'))
+
+
+function getOldYear(){
+   while(true) {
+      const res = +promt("Сколько тебе лет?")
+      if(res === 0 || res) {
+         alert("Вам " + res + "лет")
+      }
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
